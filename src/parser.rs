7ccab049e1d0029ea6567e9baccb5dfcbb76e3d4
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-10-03 17:14:39 -0400

    fix: more complex parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,10 @@
+use nom::bytes::complete::take_until;
 use nom::character::complete::none_of;
 use nom::combinator::recognize;
-use nom::multi::{many1, many0};
+use nom::multi::{many0, many1};
 use nom::sequence::preceded;
 use nom::{
-    branch::alt,
-    bytes::complete::tag,
-    character::complete::{alphanumeric1, multispace0},
-    sequence::delimited,
+    branch::alt, bytes::complete::tag, character::complete::multispace0, sequence::delimited,
     IResult,
 };
 
@@ -19,29 +17,32 @@ pub enum TemplateNode {
 
 fn variable_parser(input: &str) -> IResult<&str, TemplateNode> {
     let (input, _) = tag("{{")(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, variable_name) = alphanumeric1(input)?;
-    let (input, _) = multispace0(input)?;
+    let (input, variable_name) = take_until("}}")(input)?;
     let (input, _) = tag("}}")(input)?;
 
-    Ok((input, TemplateNode::Variable(variable_name.to_string())))
+    println!("Input Variable: {input} - Var Name: {variable_name}");
+
+    Ok((
+        input,
+        TemplateNode::Variable(variable_name.trim().to_string()),
+    ))
 }
 
-fn function_parser(input: &str) -> IResult<&str, TemplateNode> {
-    let (input, _) = tag("{{")(input)?;
+fn internal_function_parser(input: &str) -> IResult<&str, TemplateNode> {
     let (input, _) = multispace0(input)?;
-    let (input, function_name) = alphanumeric1(input)?;
+    let (input, function_name) = recognize(many1(none_of("({{}}")))(input)?;
     let (input, _) = multispace0(input)?;
     let (input, arguments) = delimited(
         tag("("),
         many0(preceded(
             multispace0,
-            alt((variable_parser, function_parser)),
+            alt((internal_function_parser, argument_parser)),
         )),
         tag(")"),
     )(input)?;
+
+    println!("FunctionName: {function_name} - Args: {arguments:?} - input: {input}");
     let (input, _) = multispace0(input)?;
-    let (input, _) = tag("}}")(input)?;
 
     Ok((
         input,
@@ -49,13 +50,29 @@ fn function_parser(input: &str) -> IResult<&str, TemplateNode> {
     ))
 }
 
+// fn function_parser(input: &str) -> IResult<&str, TemplateNode> {
+//     let (input, _) = tag("{{")(input)?;
+//     let (input, func) = internal_function_parser(input)?;
+//     let (input, _) = tag("}}")(input)?;
+//
+//     Ok((input, func))
+// }
+
+fn argument_parser(input: &str) -> IResult<&str, TemplateNode> {
+    let (input, arg) = recognize(many1(none_of("),{{}}")))(input)?;
+    println!("Input Arg: {input} - ArgName: {arg}");
+    Ok((input, TemplateNode::Variable(arg.to_string())))
+}
+
 fn text_parser(input: &str) -> IResult<&str, TemplateNode> {
-    let (input, text) = recognize(many1(none_of("{{")))(input)?;
+    let (input, text) = take_until("{{")(input)?;
+    println!("Input Text: {input} - Text: {text}");
     Ok((input, TemplateNode::Text(text.to_string())))
 }
 
 pub fn parser(input: &str) -> IResult<&str, Vec<TemplateNode>> {
-    many1(alt((function_parser, variable_parser, text_parser)))(input)
+    println!("Start Parser: {input}");
+    many0(alt((text_parser, variable_parser, function_parser)))(input)
 }
 
 #[cfg(test)]
@@ -66,6 +83,8 @@ mod test {
     fn valid_syntax() {
         let s = "Hello {{ variable1 }}";
         let res = parser(s);
+
+        println!("{res:?}");
 
         assert!(res.is_ok());
     }
@@ -91,29 +110,20 @@ mod test {
         let s = "Hello {} } {{ variable1";
         let res = parser(s);
 
-        assert!(res.is_ok());
+        assert!(res.is_err());
     }
 
     #[test]
-    /// This check the sintax with just {}, this cannot be interpretter
-    fn valid_syntax_simple() {
+    fn invalid_syntax_simple() {
         let s = "Hello { variable1 }";
         let res = parser(s);
 
-        assert!(res.is_ok());
+        assert!(res.is_err());
     }
 
     #[test]
     fn function_syntax() {
-        let s = "Hello {{ toLoweCase(variable1) }}";
-        let res = parser(s);
-
-        assert!(res.is_ok());
-    }
-
-    #[test]
-    fn recursive_function_syntax() {
-        let s = "Hello {{ toLoweCase(trim(split(variable1, \",\"))) }}";
+        let s = "Hello {{ toLowerCase(variable1) }}";
         let res = parser(s);
 
         assert!(res.is_ok());
@@ -123,6 +133,7 @@ mod test {
     fn test_variable_parser() {
         let input = "{{ variable }}";
         let result = variable_parser(input);
+
         assert_eq!(
             result,
             Ok(("", TemplateNode::Variable("variable".to_string())))
@@ -144,6 +155,58 @@ mod test {
                         vec![TemplateNode::Variable("variable".to_string())]
                     )]
                 )
+            ))
+        );
+    }
+
+    #[test]
+    fn test_function_multiple_param() {
+        let input = "{{ toLowerCase(trim(variable), variable1, variable2) }}";
+        let result = function_parser(input);
+        assert_eq!(
+            result,
+            Ok((
+                "",
+                TemplateNode::Function(
+                    "toLowerCase".to_string(),
+                    vec![
+                        TemplateNode::Function(
+                            "trim".to_string(),
+                            vec![TemplateNode::Variable("variable".to_string()),]
+                        ),
+                        TemplateNode::Variable("variable1".to_string()),
+                        TemplateNode::Variable("variable2".to_string()),
+                    ]
+                )
+            ))
+        );
+    }
+
+    #[test]
+    fn recursive_function_syntax() {
+        let s = "Hello {{ toLowerCase(trim(split(variable1, \",\"))) }}";
+        let res = parser(s);
+
+        assert_eq!(
+            res,
+            Ok((
+                "",
+                vec![
+                    TemplateNode::Text("Hello".to_string()),
+                    TemplateNode::Function(
+                        "toLowerCase".to_string(),
+                        vec![TemplateNode::Function(
+                            "trim".to_string(),
+                            vec![TemplateNode::Function(
+                                "split".to_string(),
+                                vec![
+                                    TemplateNode::Variable("variable1".to_string()),
+                                    TemplateNode::Variable(",".to_string())
+                                ]
+                            )]
+                        )]
+                    )
+                ]
             ))
         );
     }
