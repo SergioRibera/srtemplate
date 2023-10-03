15c3283a199f617cba396898d1d9fb0e7764a9d5
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-10-03 19:39:40 -0400

    feat: add examples

---
+++ examples/custom_func.rs
@@ -0,0 +1,23 @@
+use srtemplate::SrTemplate;
+
+fn to_title(args: Vec<String>) -> String {
+    println!("Args: {args:?}");
+    args.iter()
+        .map(|a| {
+            let first = a.get(0..1).unwrap_or_default().to_uppercase();
+            let last = a.get(1..).unwrap_or_default().to_lowercase();
+            format!("{first}{last}")
+        })
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn main() {
+    let mut ctx = SrTemplate::default();
+    ctx.add_variable("var", "mUnDo".to_string());
+    ctx.add_function("toTitle", to_title);
+    
+    let template = "Hola {{ toTitle(var) }}";
+    
+    println!("Rendered: {}", ctx.render(template).unwrap());
+}
