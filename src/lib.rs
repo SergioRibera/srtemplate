1cd25f4d25d388804c49f5c578f71a06ca2e44e0
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2023-10-03 18:49:56 -0400

    feat: expose error struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ mod parser;
 mod render;
 mod template;
 
+pub use error::SrTemplateError;
 pub use template::{SrTemplate, TemplateFunction};
 
 pub mod prelude {
     pub use super::builtin::*;
+    pub use super::error::SrTemplateError;
     pub use super::{SrTemplate, TemplateFunction};
 }
