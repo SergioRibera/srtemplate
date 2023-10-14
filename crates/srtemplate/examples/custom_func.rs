e4f10db3754a7219a7fcb3b155ed2f69a1fb1114
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2023-10-14 12:48:28 -0400

    fix: update custom function example

--- crates/srtemplate/examples/custom_func.rs
+++ crates/srtemplate/examples/custom_func.rs
@@ -1,23 +1,27 @@
+use srtemplate::prelude::{validations, FuncResult};
 use srtemplate::SrTemplate;
 
-fn to_title(args: Vec<String>) -> String {
+fn to_title(args: Vec<String>) -> FuncResult {
+    validations::args_min_len(&args, 1)?; // We validate that we receive a minimum of 1 argument.
+
     println!("Args: {args:?}");
-    args.iter()
+    Ok(args
+        .iter()
         .map(|a| {
             let first = a.get(0..1).unwrap_or_default().to_uppercase();
             let last = a.get(1..).unwrap_or_default().to_lowercase();
             format!("{first}{last}")
         })
         .collect::<Vec<String>>()
-        .join(" ")
+        .join(" "))
 }
 
 fn main() {
     let mut ctx = SrTemplate::default();
     ctx.add_variable("var", &"mUnDo");
     ctx.add_function("toTitle", to_title);
-    
+
     let template = "Hola {{ toTitle(var) }}";
-    
+
     println!("Rendered: {}", ctx.render(template).unwrap());
 }
