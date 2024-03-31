bce3521f59438fde26952259c5c2b180ec0a19df
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-03-31 01:57:59 -0400

    chore(nix): add nix environment for web example

---
+++ examples/real_world/default.nix
@@ -0,0 +1,50 @@
+let
+  inherit
+    (builtins)
+    currentSystem
+    fromJSON
+    readFile
+    ;
+  getFlake = name:
+    with (fromJSON (readFile ../flake.lock)).nodes.${name}.locked; {
+      inherit rev;
+      outPath = fetchTarball {
+        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        sha256 = narHash;
+      };
+    };
+in
+{ system ? currentSystem
+, pkgs ? import (getFlake "nixpkgs") { localSystem = { inherit system; }; }
+, crane
+, cranix
+, fenix
+, ...
+}:
+let
+  # fenix: rustup replacement for reproducible builds
+  toolchain = fenix.${system}.fromToolchainFile {
+    file = ./rust-toolchain.toml;
+    sha256 = "sha256-0npjLjvxhiQcBh+W9URwg0BQN7CuItPEtsRAgOb6/1o=";
+  };
+  # crane: cargo and artifacts manager
+  craneLib = crane.${system}.overrideToolchain toolchain;
+  # cranix: extends crane building system with workspace bin building and Mold + Cranelift integrations
+  cranixLib = craneLib.overrideScope' (cranix.${system}.craneOverride);
+in
+{
+  # `nix develop`
+  devShells.default = cranixLib.devShell {
+    packages = with pkgs;
+      [
+        nodejs
+        toolchain
+        pkg-config
+        sass
+        trunk
+        leptosfmt
+        cargo-make
+        binaryen
+      ];
+  };
+}
