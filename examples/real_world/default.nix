9e544d1855ea281ac4ee4a42a913c74cc50c4665
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-06-27 20:13:24 -0400

    chore: update rust version

--- examples/real_world/default.nix
+++ examples/real_world/default.nix
@@ -25,7 +25,7 @@ let
   # fenix: rustup replacement for reproducible builds
   toolchain = fenix.${system}.fromToolchainFile {
     file = ./rust-toolchain.toml;
-    sha256 = "sha256-3St/9/UKo/6lz2Kfq2VmlzHyufduALpiIKaaKX4Pq0g=";
+    sha256 = "sha256-Ngiz76YP4HTY75GGdH2P+APE/DEIx2R/Dn+BwwOyzZU=";
   };
   # crane: cargo and artifacts manager
   craneLib = crane.${system}.overrideToolchain toolchain;
