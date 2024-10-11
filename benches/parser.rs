b00134f2b5b47bde5c1766c4d22ddd0afa7cc2ab
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-10-10 23:10:34 -0400

    feat: use divan for benchmarks

--- benches/parser.rs
+++ benches/parser.rs
@@ -1,41 +1,37 @@
-#![feature(test)]
-
-extern crate test;
-
+use divan::Bencher;
 use srtemplate::SrTemplate;
-use test::Bencher;
 
-#[bench]
-fn bench_single_instance(b: &mut Bencher) {
+fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn bench_single_instance(b: Bencher) {
     let input = "This is some text. {{ variable }} and {{ toLower(trim(variable)) }}";
 
     let ctx = SrTemplate::default();
     ctx.add_variable("variable", &"Variable");
 
-    b.iter(|| {
+    b.bench(|| {
         ctx.render(input).unwrap();
     })
 }
 
-#[bench]
-fn bench_iter_instance(b: &mut Bencher) {
-    let input = "This is some text. {{ variable }} and {{ toLower(trim(variable)) }}";
-
-    b.iter(|| {
-        let ctx = SrTemplate::default();
-        ctx.add_variable("variable", &"Variable");
-        ctx.render(input).unwrap();
-    })
+#[divan::bench(args = ["This is some text. {{ variable }} and {{ toLower(trim(variable)) }}"])]
+fn bench_iter_instance(input: &str) {
+    let ctx = SrTemplate::default();
+    ctx.add_variable("variable", &"Variable");
+    ctx.render(input).unwrap();
 }
 
-#[bench]
-fn bench_clone_instance(b: &mut Bencher) {
+#[divan::bench]
+fn bench_clone_instance(b: Bencher) {
     let input = "This is some text. {{ variable }} and {{ toLower(trim(variable)) }}";
 
     let ctx = SrTemplate::default();
     ctx.add_variable("variable", &"Variable");
 
-    b.iter(|| {
+    b.bench(|| {
         for _i in 0..100 {
             let ctx = ctx.clone();
             std::thread::spawn(move || {
