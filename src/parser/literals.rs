ec533cb972cf8060e3aef5b7a2bf673b66538fe2
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-12-26 12:42:48 -0400

    test: add tests for literals parse

--- src/parser/literals.rs
+++ src/parser/literals.rs
@@ -89,3 +89,163 @@ pub fn number_literal<'a>(
 
     Ok(TemplateNode::Number(&input[start..*position]))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup<'a>(input: &'a str) -> (&'a str, Vec<u8>, usize, usize, usize, usize) {
+        let chars = input.as_bytes();
+        println!("Input: {input}");
+        (input, chars.to_vec(), 0, 1, 0, 0)
+    }
+
+    #[test]
+    fn test_string_literal_success() {
+        let (input, chars, mut position, mut line, mut column, mut start_line) =
+            setup("\"hello world\"");
+        let result = string_literal(
+            input,
+            &chars,
+            &mut position,
+            &mut line,
+            &mut column,
+            &mut start_line,
+        );
+
+        assert!(result.is_ok());
+        let node = result.unwrap();
+        if let TemplateNode::String(value) = node {
+            assert_eq!(value, "hello world");
+        } else {
+            panic!("Expected a String node");
+        }
+        assert_eq!(position, 13);
+    }
+
+    #[test]
+    fn test_string_literal_escaped_quote() {
+        let (input, chars, mut position, mut line, mut column, mut start_line) =
+            setup(r#""hello \"world\"""#);
+        let result = string_literal(
+            input,
+            &chars,
+            &mut position,
+            &mut line,
+            &mut column,
+            &mut start_line,
+        );
+
+        assert!(result.is_ok());
+        let node = result.unwrap();
+        if let TemplateNode::String(value) = node {
+            assert_eq!(value, r#"hello \"world\""#);
+        } else {
+            panic!("Expected a String node");
+        }
+        assert_eq!(position, 17);
+    }
+
+    #[test]
+    fn test_string_literal_unterminated() {
+        let (input, chars, mut position, mut line, mut column, mut start_line) =
+            setup("\"hello world");
+        let result = string_literal(
+            input,
+            &chars,
+            &mut position,
+            &mut line,
+            &mut column,
+            &mut start_line,
+        );
+
+        assert!(result.is_err());
+
+        if let SrTemplateError::BadSyntax(error) = result.unwrap_err() {
+            assert_eq!(&error.description, "Unterminated string literal");
+        }
+        assert_eq!(position, 12);
+    }
+
+    #[test]
+    fn test_number_literal_integer() {
+        let (input, chars, mut position, mut line, mut column, mut start_line) = setup("12345");
+        let result = number_literal(
+            input,
+            &chars,
+            &mut position,
+            &mut line,
+            &mut column,
+            &mut start_line,
+        );
+
+        assert!(result.is_ok());
+        let node = result.unwrap();
+        if let TemplateNode::Number(value) = node {
+            assert_eq!(value, "12345");
+        } else {
+            panic!("Expected a Number node");
+        }
+        assert_eq!(position, 5);
+    }
+
+    #[test]
+    fn test_number_literal_float() {
+        let (input, chars, mut position, mut line, mut column, mut start_line) = setup("123.45");
+        let result = number_literal(
+            input,
+            &chars,
+            &mut position,
+            &mut line,
+            &mut column,
+            &mut start_line,
+        );
+
+        assert!(result.is_ok());
+        let node = result.unwrap();
+        if let TemplateNode::Float(value) = node {
+            assert_eq!(value, "123.45");
+        } else {
+            panic!("Expected a Float node");
+        }
+        assert_eq!(position, 6);
+    }
+
+    #[test]
+    fn test_number_literal_multiple_dots() {
+        let (input, chars, mut position, mut line, mut column, mut start_line) = setup("123.45.67");
+        let result = number_literal(
+            input,
+            &chars,
+            &mut position,
+            &mut line,
+            &mut column,
+            &mut start_line,
+        );
+
+        assert!(result.is_err());
+        if let SrTemplateError::BadSyntax(error) = result.unwrap_err() {
+            assert_eq!(&error.description, "The float just need one '.'");
+        }
+        assert_eq!(position, 6);
+    }
+
+    #[test]
+    fn test_number_literal_invalid_character() {
+        let (input, chars, mut position, mut line, mut column, mut start_line) = setup("123a45");
+        let result = number_literal(
+            input,
+            &chars,
+            &mut position,
+            &mut line,
+            &mut column,
+            &mut start_line,
+        );
+
+        assert!(result.is_err());
+        if let SrTemplateError::BadSyntax(error) = result.unwrap_err() {
+            assert_eq!(&error.description, "Invalid character in Number literal");
+        }
+        assert_eq!(position, 3);
+    }
+}
