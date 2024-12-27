e36d1a01a2a90cf412cb0c94e012a8a54b86c2a9
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-12-27 02:02:09 -0400

    feat: add better error handling and feedback to webpage

--- examples/real_world/default.nix
+++ examples/real_world/default.nix
@@ -1,23 +1,9 @@
 let
-  inherit
-    (builtins)
-    currentSystem
-    fromJSON
-    readFile
-    ;
-  getFlake = name:
-    with (fromJSON (readFile ../flake.lock)).nodes.${name}.locked; {
-      inherit rev;
-      outPath = fetchTarball {
-        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-        sha256 = narHash;
-      };
-    };
+  inherit (builtins) currentSystem;
 in
 { system ? currentSystem
-, pkgs ? import (getFlake "nixpkgs") { localSystem = { inherit system; }; }
+, pkgs
 , crane
-, cranix
 , fenix
 , ...
 }:
@@ -25,16 +11,15 @@ let
   # fenix: rustup replacement for reproducible builds
   toolchain = fenix.${system}.fromToolchainFile {
     file = ./rust-toolchain.toml;
-    sha256 = "sha256-Ngiz76YP4HTY75GGdH2P+APE/DEIx2R/Dn+BwwOyzZU=";
+    sha256 = "sha256-s1RPtyvDGJaX/BisLT+ifVfuhDT1nZkZ1NcK8sbwELM=";
   };
   # crane: cargo and artifacts manager
-  craneLib = crane.${system}.overrideToolchain toolchain;
+  craneLib = crane.overrideToolchain toolchain;
   # cranix: extends crane building system with workspace bin building and Mold + Cranelift integrations
-  cranixLib = craneLib.overrideScope' (cranix.${system}.craneOverride);
 in
 {
   # `nix develop`
-  devShells.default = cranixLib.devShell {
+  devShells.default = craneLib.devShell {
     packages = with pkgs;
       [
         nodejs
