2beb9c5d9eb7417f889077c8554492bbd8fcde0f
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-02-07 01:02:29 -0400

    feat: add default math operation functions

--- src/builtin/math.rs
+++ src/builtin/math.rs
@@ -42,6 +42,37 @@ macro_rules! gen_math_fn {
             }
         )*
     };
+    (@$name: ident, $t: ty) => {
+        /// Perform arithmetic operations on a list of values and return the result as a string.
+        ///
+        /// This function takes a slice of strings, attempts to parse them as values of type `$t`,
+        /// and then applies the `$name` operation to those values.
+        ///
+        /// # Arguments
+        ///
+        /// * `args`: A slice of strings representing values to perform the operation on.
+        ///
+        /// # Returns
+        ///
+        /// * A [`FuncResult`] containing the result of the operation as a string.
+        ///
+        /// # Errors
+        ///
+        /// This function can return an error of [`crate::function::FunctionError`] variant:
+        /// - `crate::function::FunctionError::InvalidType` if any argument cannot be parsed as a value of type `$t`.
+        /// - `crate::function::FunctionError::ConvertArgsFailed` if parsing or conversion from arguments failed.
+        #[cfg_attr(docsrs, doc(cfg(feature = "math")))]
+        #[cfg(feature = "math")]
+        pub fn $name(args: &[String]) -> FuncResult {
+            for arg in args {
+                validations::arg_type::<$t>(arg.clone())?;
+            }
+            Ok(args.iter()
+                .map(|a| a.parse::<$t>().unwrap_or_default())
+                .fold(<$t>::default(), |acc, x| acc.$name(x))
+                .to_string())
+        }
+    };
 }
 
 gen_math_fn!(add, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
@@ -49,14 +80,24 @@ gen_math_fn!(sub, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
 gen_math_fn!(mul, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
 gen_math_fn!(div, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
 
+gen_math_fn!(@add, i32);
+gen_math_fn!(@sub, i32);
+gen_math_fn!(@mul, i32);
+gen_math_fn!(@div, i32);
+
 #[doc(hidden)]
 #[macro_export]
 macro_rules! gen_math_use {
     ($tmp:ident) => {
+        gen_math_use!(@$tmp, add);
+        gen_math_use!(@$tmp, sub);
+        gen_math_use!(@$tmp, mul);
+        gen_math_use!(@$tmp, div);
         gen_math_use!($tmp, add, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
         gen_math_use!($tmp, sub, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
         gen_math_use!($tmp, mul, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
         gen_math_use!($tmp, div, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
+
     };
 
     ($tmp: ident, $name: ident, $( $t: ty ),* ) => {
@@ -65,5 +106,8 @@ macro_rules! gen_math_use {
                 $tmp.add_function(stringify!([<$name _ $t>]), builtin::math::[<$name _ $t>]);
             }
         )*
+    };
+    (@$tmp: ident, $name: ident) => {
+        $tmp.add_function(stringify!($name), builtin::math::$name);
     };
 }
